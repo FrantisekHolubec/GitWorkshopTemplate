25bfba1167ef513ab3348472840536c85de543b2
František Holubec <frantisek.holubec@gmail.com>
2023-06-06 07:29:20 +0200

    Lovely commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Debug.Log("Yes! Game just started");
+        // This game is lovely
     }
 
     private void OnDestroy()
