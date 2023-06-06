48564784f81e761701daa4e31653ee7508369c0a
František Holubec <frantisek.holubec@gmail.com>
2023-06-06 07:31:31 +0200

    Fancy texts in manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,12 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Debug.Log("Yes! Game just started");
+        // I think I want some text here
     }
 
     private void OnDestroy()
     {
         Debug.Log("Oh no! Game closed");
+        // And I also want some text here tho!
     }
 }
